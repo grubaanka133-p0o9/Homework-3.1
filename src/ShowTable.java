683ff68755ec60796f26e4f87b20025596b1f917
Anna Biga <aniabiga040896@gmail.com>
2020-01-23 23:16:54 +0100

    Request 1.2 done

--- src/ShowTable.java
+++ src/ShowTable.java
@@ -1,14 +1,8 @@
 public class ShowTable {
     public static void main(String[] args) {
-        Table table1 = new Table();
-        table1.producer = "IKEA";
-        table1.colour = "white";
-        table1.price = 180;
+        Table table1 = new Table("IKEA","white", 180);
+        Table table2 = new Table("Bodzio","brown", 300);
 
-        Table table2 = new Table();
-        table2.producer = "Bodzio";
-        table2.colour = "brown";
-        table2.price = 300;
 
         System.out.println("Table 1" + "\n" +"producer: " + table1.producer + "\n" + "colour: " + table1.colour + "\n"
         + "price: " + table1.price + "\n");
--- src/Table.java
+++ src/Table.java
@@ -2,5 +2,11 @@ public class Table {
     String producer;
     String colour;
     int price;
+
+    Table(String p, String col, int pr){
+        producer=p;
+        colour=col;
+        price=pr;
+    }
     }
 
